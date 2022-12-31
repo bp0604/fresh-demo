ec2f9b52642e6b0ee9b1f8f2580b1783a5ee7dbc
baopeng@mac <baopeng@mac.com>
2022-12-31 17:57:12 +0800

    nginx ok

--- README.md
+++ README.md
@@ -9,3 +9,11 @@ deno task start
 ```
 
 This will watch the project directory and restart as necessary.
+
+
+# Nginx
+
+OK
+fresh.dev/api/uuid
+http://fresh.dev/api/bookmarks?query=spring&field=content
+
--- services/elastic.tsx
+++ services/elastic.tsx
@@ -1,7 +1,7 @@
 import { Client as ElasticsearchClient } from "https://deno.land/x/elasticsearch@v8.3.3/mod.ts";
 import type { SearchResponse } from "https://deno.land/x/elasticsearch@v8.3.3/mod.ts";
 
-const ELASTICSEARCH_HOSTS =Deno.env.get("ELASTICSEARCH_HOSTS") || "http://192.168.2.26:9200"
+const ELASTICSEARCH_HOSTS =Deno.env.get("ELASTICSEARCH_HOSTS") || "http://192.168.2.44:9200"
 console.log("ELASTICSEARCH_HOSTS: ", ELASTICSEARCH_HOSTS)
 
 const client = new ElasticsearchClient({
